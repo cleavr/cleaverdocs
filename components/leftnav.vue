ffcf1ec6925aa5b0bf622bac487f34eab93229de
Adam Miedema <armgitaar@yahoo.com>
2020-12-10 16:52:19 -0700

    for demo

--- components/leftnav.vue
+++ components/leftnav.vue
@@ -1,54 +1,12 @@
 <template>
   <div class="leading-6 text-sm">
     <h3 class="uppercase text-gray-700 font-bold py-1">Getting Started</h3>
-    <a href="/flash-deployments" class="text-gray-500 hover:text-gray-700 group flex items-center py-1 pr-5">
+    <a href="/hello" class="text-gray-500 hover:text-gray-700 group flex items-center py-1 pr-5">
       Flash Deployments
     </a>
-    <a href="/web-apps" class="text-gray-500 hover:text-gray-700 group flex items-center py-1 pr-5">
+    <a href="/there" class="text-gray-500 hover:text-gray-700 group flex items-center py-1 pr-5">
       Web Apps
     </a>
-    <a href="/" class="text-gray-500 hover:text-gray-700 group flex items-center py-1 pr-5">
-      Web Apps
-    </a>
-    <a href="/" class="text-gray-500 hover:text-gray-700 group flex items-center py-1 pr-5">
-      Web Apps
-    </a>
-    <a href="/" class="text-gray-500 hover:text-gray-700 group flex items-center py-1 pr-5">
-      Web Apps
-    </a>
-    <h3 class="uppercase text-gray-700 font-bold py-1">Getting Started</h3>
-    <a href="/flash-deployments" class="text-gray-500 hover:text-gray-700 group flex items-center py-1 pr-5">
-      Flash Deployments
-    </a>
-    <a href="/web-apps" class="text-gray-500 hover:text-gray-700 group flex items-center py-1 pr-5">
-      Web Apps
-    </a>
-    <a href="/" class="text-gray-500 hover:text-gray-700 group flex items-center py-1 pr-5">
-      Web Apps
-    </a>
-    <a href="/" class="text-gray-500 hover:text-gray-700 group flex items-center py-1 pr-5">
-      Web Apps
-    </a>
-    <a href="/" class="text-gray-500 hover:text-gray-700 group flex items-center py-1 pr-5">
-      Web Apps
-    </a>
-    <h3 class="uppercase text-gray-700 font-bold py-1">Getting Started</h3>
-    <a href="/flash-deployments" class="text-gray-500 hover:text-gray-700 group flex items-center py-1 pr-5">
-      Flash Deployments
-    </a>
-    <a href="/web-apps" class="text-gray-500 hover:text-gray-700 group flex items-center py-1 pr-5">
-      Web Apps
-    </a>
-    <a href="/" class="text-gray-500 hover:text-gray-700 group flex items-center py-1 pr-5">
-      Web Apps
-    </a>
-    <a href="/" class="text-gray-500 hover:text-gray-700 group flex items-center py-1 pr-5">
-      Web Apps
-    </a>
-    <a href="/" class="text-gray-500 hover:text-gray-700 group flex items-center py-1 pr-5">
-      Web Apps
-    </a>
-
   </div>
 
 </template>
