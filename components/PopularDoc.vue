30b7ff8bfadf9a38ca93aef3cdaaa5bede540a31
Adam Miedema <armgitaar@yahoo.com>
2020-12-14 08:40:28 -0700

    maybe fix buttons

--- components/PopularDoc.vue
+++ components/PopularDoc.vue
@@ -1,9 +1,9 @@
 <template>
-  <nuxt-link :to="linkTo" class="transition duration-500 col-span-1 flex shadow-sm rounded-md text-white bg-gradient-to-tr
-            transform cursor-pointer py-3"
+  <nuxt-link :to="linkTo" class="col-span-1 flex shadow-sm rounded-md text-white bg-gradient-to-tr
+            transform py-3 focus:outline-none transition duration-500"
             :class="`from-${fromColor} to-${toColor} hover:from-${toColor} hover:to-${fromColor}`"
   >
-    <div class="flex-shrink-0 flex items-center justify-center w-16  text-sm font-medium rounded-l-md">
+    <div class="flex-shrink-0 flex items-center justify-center w-16 text-sm font-medium rounded-l-md">
       <icon :name="icon" class="h-5 w-5 text-white fill-white" stroke="2" />
     </div>
     <div class="flex-1 flex items-center justify-between rounded-r-md truncate">
