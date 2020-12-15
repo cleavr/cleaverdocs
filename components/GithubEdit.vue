2fe8d19d7f388c3a2d54e258d9e9ee709168400c
Adam Miedema <armgitaar@yahoo.com>
2020-12-14 19:38:36 -0700

    gh edit link update and search coming soon message

--- components/GithubEdit.vue
+++ components/GithubEdit.vue
@@ -1,5 +1,5 @@
 <template>
-  <a :href="`https://github.com/LittleBets/cleaverdocs/blob/master/content/documentation/${page}.md`"
+  <a :href="`https://github.com/cleavr/cleaverdocs/blob/master/content/${section}/${page}.md`"
      class="text-yellow-600 text-sm hover:text-yellow-700"
      target="_blank"
   >
@@ -11,6 +11,18 @@
 export default {
   props: {
     page: {required : false}
+  },
+  data () {
+    return {
+      section: ''
+    }
+  },
+  mounted () {
+    if (this.$nuxt._route.name === 'doc') {
+      this.section = 'documentation'
+    } else {
+      this.section = 'guides'
+    }
   }
 }
 </script>
--- components/Search.vue
+++ components/Search.vue
@@ -9,7 +9,7 @@
             <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
           </svg>
         </div>
-        <input id="search_field" class="block w-full h-full pl-8 pr-3 py-2 border-transparent text-gray-900 placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm" placeholder="Search" type="search" name="search">
+        <input id="search_field" class="block w-full h-full pl-8 pr-3 py-2 border-transparent text-gray-900 placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm" placeholder="coming soon..." type="search" name="search">
       </div>
     </form>
   </div>
