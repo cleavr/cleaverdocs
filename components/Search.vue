e36a9e9baa6535314a5f7acf0d0293447aa5fa65
Adam Miedema <armgitaar@yahoo.com>
2021-01-03 22:18:45 -0700

    added up / down / enter keyboard controls for search results

--- components/Search.vue
+++ components/Search.vue
@@ -13,18 +13,21 @@
         <div slot-scope="{ currentRefinement, indices, refine }">
           <input
             type="search"
-            id="searchInput"
+            ref="searchInput"
             class="w-full py-2 px-3"
             :value="currentRefinement"
             :placeholder="searchPlaceholder"
             autocomplete="off"
             @input="refine($event.currentTarget.value)"
             @focus="showResults = true"
+            @keydown.up.prevent="highlightPrevious"
+            @keydown.down.prevent="highlightNext"
+            @keydown.enter="goToDoc(indices)"
           >
           <div v-if="currentRefinement.length && showResults" class="absolute z-10 transform mt-3 px-2 w-screen max-w-md sm:px-0">
             <div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
               <div class="relative grid gap-6 bg-gray-900 text-gray-100 px-5 py-6 sm:gap-8 sm:p-8">
-                <div v-if="!indices[0].hits.length && !indices[1].hits.length" class="text-sm">
+                <div v-if="totalResults(indices) === 0" class="text-sm">
                   <p>
                     Nothing to show... 😞
                   </p>
@@ -32,14 +35,17 @@
                     Should there be? Let us know on the <a href="https://forum.cleavr.io" target="_blank" class="text-blue-500 hover:text-blue-400">forum</a>.
                   </p>
                 </div>
-                <div v-if="currentRefinement" v-for="index in indices" :key="index.objectID" class="divide-y divide-blue-900">
-                  <div v-if="index.hits.length">
-                    <h2 class="uppercase text-orange-500 py-1 px-2">{{index.indexName}}</h2>
+                <div v-if="currentRefinement" v-for="section in indices" :key="section.objectID" class="divide-y divide-blue-900">
+                  <div v-if="section.hits.length">
+                    <h2 class="uppercase text-orange-500 py-1 px-2">{{section.indexName}}</h2>
                   </div>
-                  <nuxt-link :to="index.indexName === 'guides' ? `/guides/${hit.objectID}` : hit.objectID" v-for="hit in index.hits" :key="hit.objectID"
-                             class="block text-sm col-span-2 py-2 hover:bg-blue-800 transition ease-in-out duration-150"
+                  <nuxt-link :to="section.indexName === 'guides' ? `/guides/${hit.objectID}` : hit.objectID" v-for="(hit, index) in section.hits" :key="hit.objectID"
+                             class="block text-sm col-span-2 py-2 transition ease-in-out duration-150"
+                             :position="section.indexName === 'docs' ? index : indices[0].hits.length + index"
+                             :class="{ 'bg-blue-900': isCurrentIndex(section.indexName === 'docs' ? index : indices[0].hits.length + index)}"
                   >
-                    <div class="px-2">
+                    <div class="px-2" @mouseover="highlightedIndex = (section.indexName === 'docs' ? index : indices[0].hits.length + index)">
+
                       <ais-highlight attribute="title" :hit="hit" class="block text-blue-300 font-medium" />
                       <ais-snippet attribute="bodyPlainText" :hit="hit" class="block text-gray-100 font-base" />
                     </div>
@@ -69,14 +75,16 @@ export default {
         'EVJH6MC2UJ',
         '2410e19a2c436d23c08935d012eeca29'
       ),
-      showResults: true
+      showResults: true,
+      highlightedIndex: -1,
+      resultsCount: 0,
     };
   },
   mounted: function () {
     this.$nextTick(function () {
       window.addEventListener('keydown', event => {
         if(event.metaKey && event.key === 'k'){
-          document.getElementById('searchInput').focus()
+          this.$refs.searchInput.focus()
           event.preventDefault()
         }
       })
@@ -85,6 +93,7 @@ export default {
   watch: {
     '$route' () {
       this.showResults = false
+      this.$refs.searchInput.blur()
     },
   },
   computed: {
@@ -99,9 +108,33 @@ export default {
     }
   },
   methods: {
+    totalResults (results) {
+      this.resultsCount = results[0].hits.length + results[1].hits.length
+      return this.resultsCount
+    },
     onClickOutside () {
       this.showResults = false
     },
+    highlightPrevious() {
+      if (this.highlightedIndex > 0) {
+        this.highlightedIndex -= 1
+      }
+    },
+    highlightNext() {
+      if (this.highlightedIndex < this.resultsCount - 1) {
+        this.highlightedIndex += 1
+      }
+    },
+    isCurrentIndex (index) {
+      return index === this.highlightedIndex
+    },
+    goToDoc (indices) {
+      if (this.highlightedIndex < indices[0].hits.length) {
+        this.$nuxt.$router.push(`/${indices[0].hits[this.highlightedIndex].objectID}`)
+      } else {
+        this.$nuxt.$router.push(`/guides/${indices[1].hits[this.highlightedIndex - indices[0].hits.length].objectID}`)
+      }
+    }
   }
 };
 </script>
